d3a9b1735745a18522ccd1c99afa3f4c652a3aa4
Isox-z <theo.meunier41@gmail.com>
2021-08-05 10:16:42 +0200

    fix: add bootstrap in fullcalendar

--- resources/views/calendar/index.blade.php
+++ resources/views/calendar/index.blade.php
@@ -1,5 +1,33 @@
 @extends('layouts.app')
 
+<link href='https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.css' rel='stylesheet'/>
+<link href='https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.13.1/css/all.css' rel='stylesheet'>
+
 @section('content')
-    <h1>coucou mon calendrier</h1>
+    <div>
+        <div id='calendar-container' wire:ignore>
+            <div id='calendar'></div>
+        </div>
+    </div>
+
 @endsection()
+@push('scripts')
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.6.0/main.min.js'></script>
+    <script>
+        document.addEventListener('livewire:load', function () {
+            const Calendar = FullCalendar.Calendar;
+            const calendarEl = document.getElementById('calendar');
+            const calendar = new Calendar(calendarEl, {
+                headerToolbar: {
+                    left: 'prev',
+                    center: 'title',
+                    right: 'next'
+                },
+                locale: '{{ config('app.locale') }}',
+                themeSystem: 'bootstrap',
+            });
+            calendar.render();
+        });
+    </script>
+    <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.6.0/main.min.css' rel='stylesheet'/>
+@endpush
