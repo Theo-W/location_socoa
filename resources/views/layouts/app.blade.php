d5daa2387743c2ca5297e764f1b048b2498004d7
Isox-z <theo.meunier41@gmail.com>
2021-09-03 19:39:49 +0200

    fix: hello lagout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,18 +56,17 @@
                     @endif
                 @else
                     <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <a id="nav-link" class="nav-link" role="button">
                             {{ Auth::user()->name }}
                         </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
+                    </li>
+                    <li class="nav-item">
+                        <div>
+                            <a class="nav-link" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
-
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                 @csrf
                             </form>
