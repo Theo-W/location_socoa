586cde6caaf51f2c8b78838b742651e95b1c8d55
Isox-z <theo.meunier41@gmail.com>
2021-09-04 11:41:15 +0200

    feat: lang login fr

---
+++ resources/lang/fr/auth/login.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'title' => "Se connecter",
+    'email' => "Email",
+    'password' => 'Mot de passe',
+    'remenber_me' => "Ce souvenir de moi",
+    'button' => "Se connecter"
+];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
     <div class="container">
-        <div>{{ __('Login') }}</div>
+        <h1>{{ __('auth/login.title') }}</h1>
 
         <div>
             <form method="POST" action="{{ route('login') }}">
                 @csrf
 
                 <div class="form-group">
-                    <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                    <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('auth/login.email') }}</label>
                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
                            name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
@@ -21,7 +21,7 @@
                 </div>
 
                 <div class="form-group">
-                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('auth/login.password') }}</label>
                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
                            name="password" required autocomplete="current-password">
 
@@ -33,11 +33,11 @@
                 </div>
 
                 <div class="form-group">
-                    <div class="form-check">
+                    <div class="form-check my-2">
                         <input class="form-check-input" type="checkbox" name="remember"
                                id="remember" {{ old('remember') ? 'checked' : '' }}>
                         <label class="form-check-label" for="remember">
-                            {{ __('Remember Me') }}
+                            {{ __('auth/login.remenber_me') }}
                         </label>
                     </div>
                 </div>
@@ -45,7 +45,7 @@
                 <div class="form-group">
                     <div>
                         <button type="submit" class="btn btn-primary">
-                            {{ __('Login') }}
+                            {{ __('auth/login.button') }}
                         </button>
                     </div>
                 </div>
